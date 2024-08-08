ac0903d421d827f4af9cbd60687b3dd724c7f416
TaylorDavis16 <sheldonlee016@gmail.com>
2024-08-08 00:41:29 -0400

    update github workflow to install docker compose

--- test.sh
+++ test.sh
@@ -6,6 +6,13 @@ set -e
 # Build and run the test containers
 docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit
 
+# Retrieve the dynamically allocated port for Localstack
+LOCALSTACK_PORT=$(docker-compose -f docker-compose.test.yml port localstack 4566 | cut -d: -f2)
+
+# Set environment variables for the tests
+export DYNAMODB_ENDPOINT=http://localhost:$LOCALSTACK_PORT
+export S3_ENDPOINT=http://localhost:$LOCALSTACK_PORT
+
 # Capture the exit status of the tests
 EXIT_CODE=$(docker-compose -f docker-compose.test.yml ps -q api-test | xargs docker inspect -f '{{ .State.ExitCode }}')
 
